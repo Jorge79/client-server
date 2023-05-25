af45cb1efd490a26be8963062cf0376772411f74
Jorge79 <Jorge7.9@hotmail.com>
2023-05-25 11:44:01 -0300

    remove struct from client

--- client/client.go
+++ client/client.go
@@ -10,20 +10,6 @@ import (
 	"time"
 )
 
-type Money struct {
-	Code       string `json:"code"`
-	CodeIn     string `json:"codein"`
-	Name       string `json:"name"`
-	High       string `json:"high"`
-	Low        string `json:"low"`
-	VarBid     string `json:"varBid"`
-	PctChange  string `json:"pctChange"`
-	Bid        string `json:"bid"`
-	Ask        string `json:"ask"`
-	Timestamp  string `json:"timestamp"`
-	CreateDate string `json:"create_date"`
-}
-
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*2000)
 	defer cancel()
